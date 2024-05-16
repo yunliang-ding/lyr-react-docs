3c95e83a7e7b87491c33d057b265d06886394243
yunliang <804408086@qq.com>
2024-05-16 20:02:48 +0800

    demo

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# lyr-react-docs
+# 参看文档
 
-- 自动解析 docs 文件夹下的md文件，并将渲染的组件，制作成路由
-- md文件中的组件渲染能力，依赖 MarkdownViewer
-- 组件打包的能力，ems\cjs\umd
-- 服务快速启动
+[点击跳转文档](http://dev-ops.yunliang.cloud/website/lyr-docs)
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
+import './index.less';
 
 export const MyButton = ({ children }: { children: ReactElement }) => {
-  return <button>{children}</button>;
+  return <button className="my-btn">{children}</button>;
 };
